5860373d32e947e4ef62c7d901aa9930ccbccbd0
xiaour <cityuu@163.com>
2017-04-26 13:45:54 +0800

    白名单更新

--- SpringBootDemo/src/main/java/com/xiaour/spring/boot/filter/MyFilter.java
+++ SpringBootDemo/src/main/java/com/xiaour/spring/boot/filter/MyFilter.java
@@ -23,7 +23,7 @@ import com.alibaba.druid.support.http.StatViewServlet;
  */
 @WebServlet(urlPatterns = "/druid/*", 
 initParams={
-        @WebInitParam(name="allow",value="192.168.0.230,127.0.0.1"),// IP白名单 (没有配置或者为空，则允许所有访问)
+        @WebInitParam(name="allow",value=""),// IP白名单 (没有配置或者为空，则允许所有访问)
         @WebInitParam(name="deny",value="192.168.0.1"),// IP黑名单 (存在共同时，deny优先于allow)
         @WebInitParam(name="loginUsername",value="root"),// 用户名
         @WebInitParam(name="loginPassword",value="123456"),// 密码
